664c70eeafb99ee2be3aa858214fa03ed02ce400
DeveloperNoob <Jackbro0007@gmail.com>
2022-09-21 10:34:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Godstimep21/joeyking.git /joeyking
+  git clone https://github.com/DeveloperNoob/Zeo.git /Zeo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
