ed4c2e7233a46be1431ecec6ef7a0175793b3365
mahinwizard4444 <86041088+mahinwizard4444@users.noreply.github.com>
2022-11-14 10:49:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone git clone https://github.com/Godstimep21/joeyking.git /joeyking
+  git clone git clone https://github.com/mahinwizard4444/V4 /joeyking
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
